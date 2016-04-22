f4a3aa3b2a2528b886c2ec896f16c6634f00ce60
lzxb <1340641314@qq.com>
2016-04-22 21:28:56 +0800

    1.0.0版本

---
+++ fors.js
@@ -0,0 +1,41 @@
+/*
+    version: 1.0.0
+    date: 2016-04-22
+    author: 狼族小狈
+    github：https://github.com/1340641314/fors
+*/
+!(function (fors) {
+    if (typeof define === 'function' && typeof define.amd === 'object' && define.amd) {
+
+        define(function () {
+            return fors;
+        });
+    } else if (typeof module !== 'undefined' && module.exports) {
+        module.exports = fors;
+    } else {
+        window.fors = fors;
+    }
+})(function () {
+    var list = arguments;
+    var i = 0;
+    var query = []; //临时保存参数
+    fors();
+
+    function fors() {
+        for (i; i < list.length; i++) {
+            if (cur(list[i]) === false) {
+                break; //异步执行，等待手动执行下一个方法
+            }
+        }
+    }
+
+    function cur(fn) {
+        var next = [function () {
+            i++;
+            query = Array.prototype.slice.call(arguments);
+            fors();
+        }];
+
+        return fn.apply(this, next.concat(query), query);
+    }
+});
