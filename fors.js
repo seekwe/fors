b629d1a1c4a9bc8b7f8c53c5a17d89ff313f0af8
lzxb <1340641314@qq.com>
2016-04-28 23:09:09 +0800

    支持多支线执行

--- fors.js
+++ fors.js
@@ -1,5 +1,5 @@
 /*
-    version: 1.0.0
+    version: 1.1.0
     date: 2016-04-22
     author: 狼族小狈
     github：https://github.com/1340641314/fors
@@ -16,10 +16,10 @@
         window.fors = fors;
     }
 })(function () {
-    var list = arguments;
-    var i = 0;
-    var query = []; //临时保存参数
-    var resMethod = (function () { //保存有名字的方法，会return 出去
+    'use strict';
+    var params = Array.prototype.slice.call(arguments);
+
+    var resMethod = (function (list) {
         var obj = {};
         for (var i = 0; i < list.length; i++) {
             if (/^.+$/.test(list[i].name)) {
@@ -27,16 +27,15 @@
                     return function () {
                         return fn.apply(this, Array.prototype.slice.call(arguments)); //参数替换
                     };
-                })(list[i]);
+                }).call(this, list[i]);
             }
         }
         return obj;
-    })();
-    fors();
-    return resMethod;
+    }).call(this, params);
 
-    function fors() {
-        for (i; i < list.length; i++) {
+    function start(list) {
+        var params = Array.prototype.slice.call(arguments).slice(1);
+        for (var i = 0; i < list.length; i++) {
             var cur = (function (fn) {
                 if (typeof fn === 'function') {
                     return fn;
@@ -46,20 +45,27 @@
                     return function () {};
                 }
             })(list[i]);
-
-            if (callCur(cur) === false) {
-                break; //异步执行，等待手动执行下一个方法
+            var bool = cur.call(this, cur, list.slice(i + 1, list.length), params);
+            if (bool === false) {
+                return;
             }
         }
+    };
+
+    function cur(cb, list, params) {
+        function next() {
+            var listArr = [list];
+            var params = Array.prototype.slice.call(arguments);
+            return start.apply(this, listArr.concat(params));
+        };
+        return cb.apply(this, [next].concat(params));
     }
 
-    function callCur(fn) {
-        var next = [function () {
-            i++;
-            query = Array.prototype.slice.call(arguments);
-            fors();
-        }];
-
-        return fn.apply(this, next.concat(query), query);
+    if (params[0] !== false) {
+        start.call(this, params);
     }
+    resMethod.start = function () {
+        start.call(this, params);
+    };
+    return resMethod;
 });
